076760e89859cc0bd5ed767bd3bbb771eabed10f
Dmitry Barsukoff <rizzzhik@gmail.com>
2021-09-16 13:41:22 +0300

    [FIX] Save image path

--- HybridsRu_bot/handlers.py
+++ HybridsRu_bot/handlers.py
@@ -6,13 +6,13 @@ from aiogram import types
 
 async def save_image(message: types.Message):
     # Get current time
-    time = datetime.now()
+    dt = datetime.now()
 
     # Get path to save directory
-    save_dir = os.path.join(message.bot.get("save_dir"), time.strftime("%Y/%B/%d"))
+    save_dir = os.path.join(message.bot.get("save_dir"), dt.strftime("%Y"), dt.strftime("%B"), dt.strftime("%d"))
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
     # Save image
-    save_path = os.path.join(save_dir, time.strftime("%H:%M:%S.%f") + ".jpg")
+    save_path = os.path.join(save_dir, dt.strftime("%H:%M:%S-%f.jpg"))
     await message.photo[-1].download(save_path)
